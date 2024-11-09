6bd524a4f2fd1f5bf20fa9e4c616ed5656dda211
RicoP <RicoP@users.noreply.github.com>
2024-11-09 16:43:01 +0100

    current explorer state

--- Assets/Exploration Assets/ExplorerGridClicker.cs
+++ Assets/Exploration Assets/ExplorerGridClicker.cs
@@ -7,6 +7,10 @@ public class ExplorerGridClicker : MonoBehaviour
 {
     private Grid grid;
     private Tilemap tilemap;
+    private ExplorerTileInfo tileInfoHold;
+
+    public TileBase[] heroTiles;
+
 
     void Start()
     {
@@ -16,23 +20,59 @@ public class ExplorerGridClicker : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Detect left mouse click
+        bool isMouseLeftDown = Input.GetMouseButtonDown(0);
+        bool isMouseLeftUp = Input.GetMouseButtonUp(0);
+        //bool isMouseRight = Input.GetMouseButtonDown(1);
+        if (isMouseLeftDown) // Detect left mouse click
         {
-            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseWorldPosition.z = 0;
+            ExplorerTileInfo info = GetTileInfoAtMouse();
 
-            Vector3Int cellPosition = grid.WorldToCell(mouseWorldPosition);
-
-            // Optionally, check if a tile exists
-            TileBase clickedTile = tilemap.GetTile(cellPosition);
-            if (clickedTile != null)
+            if (info.clickedTile != null)
             {
-                Debug.Log($"Clicked on cell: {cellPosition} with tile: {clickedTile.name}");
+                Debug.Log($"Clicked on cell: {info.cellPosition} with tile: {info.clickedTile.name}");
+                tileInfoHold = info;
             }
             else
             {
-                Debug.Log($"Clicked on empty cell: {cellPosition}");
+                Debug.Log($"Clicked on empty cell: {info.cellPosition}");
+                tilemap.SetTile(info.cellPosition, heroTiles[0]);
             }
+
         }
+        if (isMouseLeftUp)
+        {
+            if (tileInfoHold?.clickedTile != null)
+            {
+                ExplorerTileInfo info = GetTileInfoAtMouse();
+                if (info.clickedTile != null)
+                { 
+                    Debug.Log($"Dragged from {tileInfoHold.clickedTile.name} to {info.clickedTile.name}");
+
+                }
+            }
+            tileInfoHold = null;
+        }
+    }
+
+    private ExplorerTileInfo GetTileInfoAtMouse()
+    {
+        Vector3 screenPosition = Input.mousePosition;
+        return GetTileInfoAtScreenPosition(screenPosition);
+    }
+
+    private ExplorerTileInfo GetTileInfoAtScreenPosition(Vector3 screenPosition)
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        mouseWorldPosition.z = 0;
+
+        Vector3Int cellPosition = grid.WorldToCell(mouseWorldPosition);
+
+        // Optionally, check if a tile exists
+        TileBase clickedTile = tilemap.GetTile(cellPosition);
+
+        ExplorerTileInfo info = new ExplorerTileInfo();
+        info.cellPosition = cellPosition;
+        info.clickedTile = clickedTile;
+        return info;
     }
 }
---
+++ Assets/Exploration Assets/ExplorerTileInfo.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+class ExplorerTileInfo
+{
+    public TileBase clickedTile;
+    public Vector3Int cellPosition;
+}
